0ff8638314f8467bec22c1f1d4cb4836c700da84
markocic98 <markodkocic@gmail.com>
2022-12-26 17:56:28 +0100

    Add project files.

---
+++ PhotoFormatConverter/PhotoFormatConverter.cs
@@ -0,0 +1,15 @@
+namespace PhotoFormatConverter
+{
+    public partial class PhotoFormatConverter : Form
+    {
+        public PhotoFormatConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void klik(object sender, EventArgs e)
+        {
+            MessageBox.Show("Radi!");
+        }
+    }
+}
