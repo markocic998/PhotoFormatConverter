9904995e23f03a96e67049cf42905422d71c1ed2
markocic98 <markodkocic@gmail.com>
2023-01-03 22:29:41 +0100

    User can open file dialog and select files or folder.

--- PhotoFormatConverter/PhotoFormatConverter.Designer.cs
+++ PhotoFormatConverter/PhotoFormatConverter.Designer.cs
@@ -28,21 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.SelectedFilesOrFolderText = new System.Windows.Forms.TextBox();
+            this.OpenFileButton = new System.Windows.Forms.Button();
+            this.FolderBrowserButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // SelectedFilesOrFolderText
+            // 
+            this.SelectedFilesOrFolderText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SelectedFilesOrFolderText.Location = new System.Drawing.Point(100, 65);
+            this.SelectedFilesOrFolderText.Multiline = true;
+            this.SelectedFilesOrFolderText.Name = "SelectedFilesOrFolderText";
+            this.SelectedFilesOrFolderText.ReadOnly = true;
+            this.SelectedFilesOrFolderText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.SelectedFilesOrFolderText.Size = new System.Drawing.Size(1000, 200);
+            this.SelectedFilesOrFolderText.TabIndex = 0;
+            this.SelectedFilesOrFolderText.Text = "There are no files or folder selected yet.";
+            // 
+            // OpenFileButton
+            // 
+            this.OpenFileButton.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OpenFileButton.Location = new System.Drawing.Point(100, 271);
+            this.OpenFileButton.Name = "OpenFileButton";
+            this.OpenFileButton.Size = new System.Drawing.Size(187, 50);
+            this.OpenFileButton.TabIndex = 1;
+            this.OpenFileButton.Text = "Select Files";
+            this.OpenFileButton.UseVisualStyleBackColor = true;
+            this.OpenFileButton.Click += new System.EventHandler(this.OpenFileButton_Click);
+            // 
+            // FolderBrowserButton
+            // 
+            this.FolderBrowserButton.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FolderBrowserButton.Location = new System.Drawing.Point(913, 271);
+            this.FolderBrowserButton.Name = "FolderBrowserButton";
+            this.FolderBrowserButton.Size = new System.Drawing.Size(187, 50);
+            this.FolderBrowserButton.TabIndex = 2;
+            this.FolderBrowserButton.Text = "Open Folder";
+            this.FolderBrowserButton.UseVisualStyleBackColor = true;
+            this.FolderBrowserButton.Click += new System.EventHandler(this.FolderBrowserButton_Click);
             // 
             // PhotoFormatConverter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(882, 453);
+            this.ClientSize = new System.Drawing.Size(1182, 453);
+            this.Controls.Add(this.FolderBrowserButton);
+            this.Controls.Add(this.OpenFileButton);
+            this.Controls.Add(this.SelectedFilesOrFolderText);
             this.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "PhotoFormatConverter";
             this.Text = "Photo Format Converter";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox SelectedFilesOrFolderText;
+        private Button OpenFileButton;
+        private Button FolderBrowserButton;
     }
 }
--- PhotoFormatConverter/PhotoFormatConverter.cs
+++ PhotoFormatConverter/PhotoFormatConverter.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace PhotoFormatConverter
 {
     public partial class PhotoFormatConverter : Form
@@ -5,6 +7,44 @@ namespace PhotoFormatConverter
         public PhotoFormatConverter()
         {
             InitializeComponent();
+        }
+
+        private void OpenFileButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Title = "My Image Browser";
+                ofd.Filter = "Image Files|*.bmp;*.jpg;*.png;*.gif;";
+                ofd.Multiselect = true;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    SelectedFilesOrFolderText.Text = DetermineSelectedFilesText(ofd.FileNames);
+                }
+            }
+
+        }
+
+        private void FolderBrowserButton_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    SelectedFilesOrFolderText.Text = DetermineSelectedFilesText(files);
+                }
+            }
+        }
+
+        private string DetermineSelectedFilesText(string[] files)
+        {
+            string text = "";
+            foreach (string file in files)
+            {
+                text += file + "\r\n";
+            }
+            return text.Substring(0, text.Length - 1);
         }
     }
 }
